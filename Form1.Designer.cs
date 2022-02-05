0a06e4e7c0c5c40f01b9d70abb0850bbc8dc239c
nisari-l <nisarimdevan@hotmail.com>
2022-02-05 12:54:19 +0530

    0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,17 +36,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(223, 69);
+            this.button1.Location = new System.Drawing.Point(328, 123);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(361, 254);
+            this.checkBox1.Location = new System.Drawing.Point(154, 254);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(95, 20);
             this.checkBox1.TabIndex = 1;
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace TollBoothManagementSystem
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congrats");
+        }
     }
 }
