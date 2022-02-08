6f0df0e154a888a0834279c6fcb6cd7b224e33ad
nisari-l <nisarimdevan@hotmail.com>
2022-02-08 23:27:56 +0530

    1

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,12 @@ namespace TollBoothManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
     }
 }
